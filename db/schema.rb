ce63650a98908f53a06b9850fe5d08c1a5082f82
Josh Brito <jbrito6492@gmail.com>
2022-09-02 22:37:51 -0700

    Behavioral - Added schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_03_052851) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "games", force: :cascade do |t|
+    t.string "away_team"
+    t.string "home_team"
+    t.datetime "gametime"
+    t.bigint "schedule_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["schedule_id"], name: "index_games_on_schedule_id"
+  end
+
+  create_table "schedules", force: :cascade do |t|
+    t.integer "week"
+    t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.string "short_name"
+    t.bigint "game_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_teams_on_game_id"
+  end
+
+  add_foreign_key "games", "schedules"
+  add_foreign_key "teams", "games"
 end
