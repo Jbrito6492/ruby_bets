00929b6c957e6d29ee95be390bcc884a3e6eec00
Josh Brito <jbrito6492@gmail.com>
2022-09-08 23:07:57 -0700

    Behaviorla - game_time property added to game factory

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :game do
     away_team { Faker::Team.name }
     home_team { Faker::Team.name }
+    game_time { Faker::Time.between(from: DateTime.now - 7.days, to: DateTime.now + 3.weeks) }
   end
 end
