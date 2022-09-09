40670fbc86fda9a4efec02b9ea9c03c313c4572c
Josh Brito <jbrito6492@gmail.com>
2022-09-08 23:46:59 -0700

    Behavioral - helper methods added and implemented

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -4,4 +4,20 @@ module GamesHelper
       raw file.read
     end
   end
+
+  def localized_game_time(game)
+    game.game_time.localtime.strftime("%A, %B %e, %Y at %l:%M %p")
+  end
+
+  def away_team(game)
+    game.teams.find_by(name: game.away_team)
+  end
+
+  def home_team(game)
+    game.teams.find_by(name: game.home_team)
+  end
+
+  def started?(game)
+    game.game_time <= DateTime.now
+  end
 end
