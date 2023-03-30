01c4a3a1be9aab40ed5cda778d24c182d2cf326c
Josh Brito <josh.brito@strongmind.com>
2023-03-29 18:27:14 -0700

    main: Commit message: Refactor NFL client to add frozen string literal and better error handling

--- lib/clients/sports_io/nfl.rb
+++ lib/clients/sports_io/nfl.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'faraday'
 
 module Clients
@@ -6,18 +8,23 @@ module Clients
       BASE_URL = 'https://api.sportsdata.io/v3/nfl/'
       attr_reader :api_key, :adapter
 
-      def initialize(api_key: ENV['SPORTSIO_API_KEY'], adapter: Faraday.default_adapter)
+      def initialize(api_key: ENV['SPORTSIO_API_KEY'])
         @api_key = api_key
-        @adapter = adapter
+      end
+
+      def headers
+        {
+          'Ocp-Apim-Subscription-Key' => @api_key
+        }
       end
 
       def connection
-        @connection ||= Faraday.new do |conn|
-          conn.url_prefix = BASE_URL
-          conn.headers['Ocp-Apim-Subscription-Key'] = api_key
-          conn.request :json
+        @connection ||= Faraday.new(BASE_URL) do |conn|
+          conn.headers = headers
+          conn.request :json, content_type: /\bjson$/
           conn.response :json, content_type: /\bjson$/
-          conn.adapter adapter
+          conn.response :raise_error, error_prefix: 'SportsIo::Nfl'
+          conn.adapter Faraday.default_adapter
         end
       end
 
@@ -26,7 +33,9 @@ module Clients
       end
 
       def games
-        connection.get("scores/json/Schedules/#{DateTime.now.year}").body.each { |game| game.deep_symbolize_keys! }
+        connection.get("scores/json/Schedules/#{DateTime.now.year}").body.each(&:deep_symbolize_keys!)
+      rescue Faraday::ClientError => e
+        Rails.logger.error "SportsIo::Nfl#games: #{e.message}"
       end
     end
   end
